7945770c02bcbfff5816880a748f485822b1d178
Chris Marslender <chrismarslender@gmail.com>
2025-07-30 11:13:06 -0500

    Add option to disable grouped updates + add for npm (#60)
    * Add option to disable grouped updates + improve debug function to test this
    
    * Add option for npm grouped updates
    
    * Retain current bahavior for npm repos by default

--- cmd/debugTemplate.go
+++ cmd/debugTemplate.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/chia-network/repo-content-updater/internal/config"
 	"github.com/chia-network/repo-content-updater/internal/repo"
 )
 
@@ -16,11 +17,20 @@ var debugTemplateCmd = &cobra.Command{
 	Use:   "debug-template",
 	Short: "Renders the given template for debugging",
 	Run: func(cmd *cobra.Command, args []string) {
+		cfg, err := config.LoadConfig(viper.GetString("config"))
+		if err != nil {
+			log.Fatalf("error loading config: %s\n", err.Error())
+		}
+
 		tmplContent, err := os.ReadFile(path.Join(viper.GetString("templates"), args[0]))
 		if err != nil {
 			log.Fatalln(err.Error())
 		}
-		content, err := repo.ProcessTemplate(tmplContent, map[string]string{}, map[string]string{})
+		content, err := repo.ProcessTemplate(
+			tmplContent,
+			cfg.Variables,
+			viper.GetStringMapString("debug-template-vars"),
+		)
 		if err != nil {
 			log.Fatalln(err.Error())
 		}
@@ -30,5 +40,8 @@ var debugTemplateCmd = &cobra.Command{
 }
 
 func init() {
+	debugTemplateCmd.PersistentFlags().StringToString("var", map[string]string{}, "Set override vars for the template")
+	cobra.CheckErr(viper.BindPFlag("debug-template-vars", debugTemplateCmd.PersistentFlags().Lookup("var")))
+
 	rootCmd.AddCommand(debugTemplateCmd)
 }
